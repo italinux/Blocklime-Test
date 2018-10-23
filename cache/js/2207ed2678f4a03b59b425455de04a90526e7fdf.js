6cf8e02c36ce2ccf0133bfa479d1251920f71922
Matteo Montanari <matteo@italinux.com>
2018-10-23 04:15:11 +0200

    Delete 2207ed2678f4a03b59b425455de04a90526e7fdf.js

--- cache/js/2207ed2678f4a03b59b425455de04a90526e7fdf.js
+++
@@ -1,2 +0,0 @@
-$(function(){var thisNavbar=$("section.fixed.menu");var fadingClass="with-fade";var fadingPosition=200;var thisOffset=0;if(thisNavbar.hasClass("loggedIn")){thisOffset+=48}fadingPosition+=thisOffset;$(window).scroll(function(){if($(this).scrollTop()>fadingPosition){thisNavbar.addClass(fadingClass)}else{thisNavbar.removeClass(fadingClass)}})});;
-
